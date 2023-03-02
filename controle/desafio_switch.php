3e29d6abdd0eb1f9f500502470fef0704780dd1d
ErikSilveira <Erikroquesilveira0988@gmail.com>
2023-03-02 06:13:22 -0300

    Conclusão do Modulo

--- controle/desafio_switch.php
+++ controle/desafio_switch.php
@@ -5,8 +5,10 @@
     <select name="conversao" id="conversao">
         <option value="km-milha"> KM > Milha</option>
         <option value="milha-Km"> Milha > KM</option>
-        <option value="metro-km"> metro > KM</option>
-        <option value="km-metro"> KM- Metros</option>
+        <option value="metro-km"> Metro > KM</option>
+        <option value="km-metro"> KM > Metros</option>
+        <option value="°C-°F."> °C > °F </option>
+        <option value="°F.-°C"> F° > °C  </option>
     </select>
     <button>Calcular</button>
 </form>
@@ -16,3 +18,44 @@
         font-size: 1.8rem;
     } 
 </style>
+
+
+<?php 
+
+$param = $_POST['param'] ?? 0;
+$resultado = 0;
+
+if(isset($_POST['param']) && isset($_POST['conversao'])){
+switch ($_POST['conversao']) {
+    case  'km-milha':
+        $resultado = $param *0.6214;
+        $mensagem = "$param KM= $resultado Milhas";
+        break;
+    case  'milha-km':
+        $resultado = $param * 1.609;
+        $mensagem = "$param Milhas= $resultado KM";
+        break;
+    case  'metro-km':
+        $resultado = $param * 0.001;
+        $mensagem = "$param Metros = $resultado KM";
+        break;  
+    case  'km-metro':
+        $resultado = $param * 1000;
+        $mensagem = "$param KM = $resultado Metros";
+        break;      
+    case  '°C-°F.':
+        $resultado = $param * 1.8 + 32;
+        $mensagem = "$param °C = $resultado °F";
+        break;
+    case  '°F.-°C':
+        $resultado = ($param - 32) / 1.8;
+        $mensagem = "$param °F = $resultado °C";
+        break;
+    default:
+        $mensagem = "Nada a fazer";
+        break;
+
+}
+}
+
+echo "<p>$mensagem</p>";
